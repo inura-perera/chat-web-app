b35e1463746460e7eb3258aa0bbba812be6cfdb7
inura-perera <inuraperera6@gmail.com>
2024-01-21 21:25:02 +0530

    Register and Login function

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,9 @@ import Button from "@/app/components/Button";
 import { DiSafari } from "react-icons/di";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
+import {toast} from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -40,17 +43,41 @@ const AuthForm =()=>{
         setIsLoading(true);
 
         if(variant==='REGISTER'){
-            //axios Register
+            axios.post('/api/register',data)
+            .catch(() =>toast.error('Something went wrong!'))
+            .finally(() =>setIsLoading(false))
         }
         if(variant==='LOGIN'){
-            //NextAuth SignIn
+            signIn('credentials',{
+                ...data,
+                redirect:false
+            })
+            .then((callback)=>{
+                if(callback?.error){
+                    toast.error('Invalid credentials');
+                }
+                if(callback?.ok && !callback?.error){
+                    toast.success('Logged in!');
+                }
+            })
+            .finally(() =>setIsLoading(false))
         }
     }
 
     const socialAction = (action:string)=>{
         setIsLoading(true);
 
-        //NextAuth Social Sign In
+        signIn(action, {redirect: false})
+        .then((callback) =>{
+            if (callback?.error){
+                toast.error('Invalid credentials');
+            }
+
+            if(callback?.ok && !callback?.error){
+                toast.success('Logged in!')
+            }
+        })
+        .finally(() =>setIsLoading(false))
     }
 
     return (
