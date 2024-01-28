14ae06519104a885137761fabf7cd94e9d37b9c2
inura-perera <inuraperera6@gmail.com>
2024-01-28 19:26:03 +0530

    final

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a university project for a Real-Time Chat App called DogeConnect featuring DOGECOIN DOGE.
+
+Technology Stack 
+
+Next.js 14
+React 
+Tailwind
+Prisma
+MongoDB
+NextAuth
+Pusher
---
+++ app/actions/getSession.ts
@@ -0,0 +1,6 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOption";
+
+export default async function getSession() {
+  return await getServerSession(authOptions);
+}
