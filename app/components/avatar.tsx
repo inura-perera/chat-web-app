a725a22332c7030bf9ce0c8b01e305ffcf8f331f
inura-perera <inuraperera6@gmail.com>
2024-01-28 16:51:21 +0530

    real time messages and active status with pusher

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -2,12 +2,17 @@
 
 import { User } from "@prisma/client";
 import Image from "next/image";
+import useActiveList from "../hooks/useActiveList";
 
 interface AvatarProps {
     user?: User;
   };
 
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
+
+    const { members } = useActiveList();
+    const isActive = members.indexOf(user?.email!) !== -1;
+
     return ( 
         <div className="relative">
             <div className="
@@ -26,7 +31,7 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
                 alt="Avatar"
             />
             </div>
-
+            {isActive ? (
             <span 
                 className="
                     absolute 
@@ -43,7 +48,7 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
                     md:w-3
                 " 
             />
-            
+            ) : null}
         </div>
      );
 }
